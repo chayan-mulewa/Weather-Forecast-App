3e43d5816a9b8ea885e24d98ca37f0aea8521c01
Chayan Mulewa <chayanmulewa@gmail.com>
2024-03-08 17:47:40 +0530

    Modify Icons

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -30,23 +30,31 @@ function WeatherForecast() {
                         setWeather(data);
                     })
                     .catch((error) => {
-                        console.log("Error : " + error);
+                        console.log("Error  : " + error);
                     });
             })
             .catch((error) => {
-                console.log("Error : " + error)
+                console.log("Error  : " + error)
                 setWeather([]);
-                alert("This is not an City");
             });
     }
+
+    function getWeatherDataIcons(iconCode) {
+        const IconAPI = "https://openweathermap.org/img/wn/" + iconCode + ".png";
+        return IconAPI;
+    }
+
 
     function searchCity(value) {
         getWeatherData(city);
     }
 
     const name = weather ? weather.name : 'N/A';
-    const temp = weather.main ? weather.main.temp + " Kelvin" : 'N/A';
-    const type = weather.weather ? weather.weather[0].main + " { " + weather.weather[0].description + " }" : 'N/A';
+    const temp = weather.main ? weather.main.temp : 'N/A';
+
+    const finalTemp = weather.main ? (temp - 273.15).toFixed(2) + " Deg" : "N/A";
+
+    const type = weather.weather ? weather.weather[0].description : 'N/A';
     const date = weather.dt ? new Date(weather.dt * 1000).toLocaleDateString() : 'N/A';
     const time = weather.dt ? new Date(weather.dt * 1000).toLocaleTimeString() : 'N/A';
 
@@ -55,6 +63,10 @@ function WeatherForecast() {
     const humidity = weather.main ? weather.main.humidity + " %" : 'N/A';
     const pressure = weather.main ? weather.main.pressure + " hPa" : 'N/A';
     const wind = weather.wind ? weather.wind.speed + " M/S" : 'N/A';
+
+    const icon = weather.weather ? weather.weather[0].icon : "N/A";
+
+    getWeatherDataIcons(icon);
 
     // return (
     //     <div className="h-2/4 w-2/4 flex flex-col gap-2 place-content-center place-items-cente">
@@ -74,15 +86,15 @@ function WeatherForecast() {
     //                 <div className='h-full w-full flex flex-row place-content-center place-items-center'>
     //                     <div className='h-full w-full flex flex-row gap-2 place-content-center place-items-center bg-black rounded-lg'>
     //                         <div className='h-full w-full flex flex-col gap-2 place-content-center p-3'>
-    //                             <h1>Location : {name}</h1>
-    //                             <h1>Temp : {temp}</h1>
-    //                             <h1>Weather Type : {type}</h1>
+    //                             <h1>Location  : {name}</h1>
+    //                             <h1>Temp  : {temp}</h1>
+    //                             <h1>Weather Type  : {type}</h1>
     //                         </div>
     //                     </div>
     //                     <div className='h-full w-full flex flex-col gap-2 place-content-center p-3 bg-teal-600 rounded-lg'>
-    //                         <h1>Date : {date}
+    //                         <h1>Date  : {date}
     //                         </h1>
-    //                         <h1>Time : {time}</h1>
+    //                         <h1>Time  : {time}</h1>
     //                     </div>
     //                 </div>
     //                 <div className='h-full w-full flex flex-row gap-2 place-content-center place-items-center bg-slate-200  text-black rounded-lg'>
@@ -108,7 +120,7 @@ function WeatherForecast() {
     //                     </div>
     //                 </div>
     //                 <div className='h-full w-full flex flex-col gap-2 place-content-center place-items-center'>
-    //                     <h1>Note : For Best Experience Search Only City Names</h1>
+    //                     <h1>Note  : For Best Experience Search Only City Names</h1>
     //                 </div>
     //             </div>
 
@@ -121,10 +133,7 @@ function WeatherForecast() {
         <div className="h-full w-full flex flex-col items-center justify-center bg-gray-400">
             <div className="p-20 bg-gray-400">
                 <h1 className="text-3xl mb-4 text-center">
-                    "Weather Forecast App"
-                </h1>
-                <h1 className="text-3xl mb-4 text-center">
-                    Develop by Chayan Mulewa
+                    Weather Forecast
                 </h1>
                 <div className="flex flex-col gap-4">
                     <div className="flex gap-4">
@@ -144,43 +153,48 @@ function WeatherForecast() {
 
                     <div className="flex flex-col md:flex-row gap-4">
                         <div className="flex-grow bg-black rounded-lg p-4">
-                            <h1 className="text-white mb-4">Location: {name}</h1>
-                            <h1 className="text-white mb-4">Temp: {temp}</h1>
-                            <h1 className="text-white">Weather Type: {type}</h1>
+                            <h1 className="text-white mb-4">Location : {name}</h1>
+                            <h1 className="text-white mb-4">Temp : {finalTemp}</h1>
+                            <h1 className="flex flex-row    place-items-center text-white">Weather Type : <img src={getWeatherDataIcons(icon)}></img> {type}</h1>
+
+
                         </div>
 
                         <div className="flex flex-col bg-teal-600 rounded-lg p-4 mt-4 md:mt-0 items-start justify-center">
-                            <h1 className="text-white mb-4">Date: {date}</h1>
-                            <h1 className="text-white">Time: {time}</h1>
+                            <h1 className="text-white mb-4">Date : {date}</h1>
+                            <h1 className="text-white">Time : {time}</h1>
                         </div>
                     </div>
 
                     <div className="flex flex-col place-content-between  bg-slate-200 text-black rounded-lg gap-4 p-4  md:flex-row gap-4">
                         <div className="flex flex-col">
-                            <h1>Sunrise</h1>
+                            <h1 className='flex flex-row place-content-start place-items-center' >Sunrise <img src='https://openweathermap.org/img/wn/01d.png'></img></h1>
                             <h1>{sunrise}</h1>
                         </div>
                         <div className="flex flex-col">
-                            <h1>Sunset</h1>
+                        <h1 className='flex flex-row place-content-start place-items-center' >Sunset <img src='https://openweathermap.org/img/wn/02d.png'></img></h1>
                             <h1>{sunset}</h1>
                         </div>
                         <div className="flex flex-col">
-                            <h1>Humidity</h1>
+                        <h1 className='flex flex-row place-content-start place-items-center' >Humidity <img src='https://openweathermap.org/img/wn/13d.png'></img></h1>
                             <h1>{humidity}</h1>
                         </div>
                         <div className="flex flex-col">
-                            <h1>Pressure</h1>
+                        <h1 className='flex flex-row place-content-start place-items-center gap-2' >Pressure <img  src='https://openweathermap.org/img/wn/01n.png'></img></h1>
                             <h1>{pressure}</h1>
                         </div>
                         <div className="flex flex-col">
-                            <h1>Wind</h1>
+                        <h1 className='flex flex-row place-content-start place-items-center' >Wind <img src='https://openweathermap.org/img/wn/50d.png'></img></h1>
                             <h1>{wind}</h1>
                         </div>
                     </div>
 
                     <div className="text-center mt-4">
-                        <h1 className="text-gray-300">
-                            Note: For the best experience, search only city names.
+                        <h1 className="text-gray-600">
+                            Note : For the best experience, search only city names.
+                        </h1>
+                        <h1 className="text-gray-600">
+                            Developer : Chayan Mulewa
                         </h1>
                     </div>
                 </div>
